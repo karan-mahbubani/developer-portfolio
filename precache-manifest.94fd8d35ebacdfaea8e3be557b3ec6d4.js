2d5053d345b3bf459068f49312e94fda4d22633f
karan-mahbubani <88630977+karan-mahbubani@users.noreply.github.com>
2021-08-28 23:39:06 +0530

    Updates

---
+++ precache-manifest.94fd8d35ebacdfaea8e3be557b3ec6d4.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb52d8c02dfa8abbfa8b7dedee3f20c",
+    "url": "/developer-portfolio/index.html"
+  },
+  {
+    "revision": "2e3b393ac0e8c0c76f9c",
+    "url": "/developer-portfolio/static/css/3.6e95b680.chunk.css"
+  },
+  {
+    "revision": "6168b5182d0570ba24a9",
+    "url": "/developer-portfolio/static/css/4.f0b7a320.chunk.css"
+  },
+  {
+    "revision": "a4422125666a84be9c80",
+    "url": "/developer-portfolio/static/css/main.8f4e6c0d.chunk.css"
+  },
+  {
+    "revision": "31aee73e774617d98a27",
+    "url": "/developer-portfolio/static/js/2.3688a7c7.chunk.js"
+  },
+  {
+    "revision": "2e3b393ac0e8c0c76f9c",
+    "url": "/developer-portfolio/static/js/3.796dd338.chunk.js"
+  },
+  {
+    "revision": "6168b5182d0570ba24a9",
+    "url": "/developer-portfolio/static/js/4.0fa85318.chunk.js"
+  },
+  {
+    "revision": "a4422125666a84be9c80",
+    "url": "/developer-portfolio/static/js/main.920a5b82.chunk.js"
+  },
+  {
+    "revision": "2ccee1ce21f64770d1c3",
+    "url": "/developer-portfolio/static/js/runtime-main.b39455e7.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developer-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developer-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/developer-portfolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/developer-portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "a41f784840703e7c39e9d5ff37133c77",
+    "url": "/developer-portfolio/static/media/developerActivity.a41f7848.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/developer-portfolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "d6a2dcd5cb04a2d44cca99b6acb5da7e",
+    "url": "/developer-portfolio/static/media/ibm_logo.d6a2dcd5.png"
+  },
+  {
+    "revision": "b1ae2adc77bafd0ab3f670d9ab06b5ca",
+    "url": "/developer-portfolio/static/media/jsFramework.b1ae2adc.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/developer-portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "a15c230b794fda4eb28f2320529072a1",
+    "url": "/developer-portfolio/static/media/manOnTable.a15c230b.svg"
+  },
+  {
+    "revision": "d73bb2bcddad315c67b738e10a345582",
+    "url": "/developer-portfolio/static/media/meta_pic.d73bb2bc.png"
+  },
+  {
+    "revision": "f041d99793f0c15a757a3d8b73d937a8",
+    "url": "/developer-portfolio/static/media/pierian-data-logo.f041d997.png"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/developer-portfolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "05b89315da86d827606f827e50c8e7ca",
+    "url": "/developer-portfolio/static/media/rahulshettyacademy-logo.05b89315.png"
+  },
+  {
+    "revision": "1591c202ea86be03349f8d565ac1f959",
+    "url": "/developer-portfolio/static/media/seetest-logo.1591c202.png"
+  },
+  {
+    "revision": "96c9d7a913fc06de9bd7e13f5cb26af9",
+    "url": "/developer-portfolio/static/media/skill.96c9d7a9.svg"
+  },
+  {
+    "revision": "7ebc764bf5f86dd0e65964bc4a734411",
+    "url": "/developer-portfolio/static/media/vskills-logo.7ebc764b.png"
+  },
+  {
+    "revision": "d0a70af7f35f5943c7e28883adbbb7fd",
+    "url": "/developer-portfolio/static/media/zero-to-mastery.d0a70af7.png"
+  }
+]);
